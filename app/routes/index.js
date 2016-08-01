89800bcc2cdad01e87f14cdf5207c48b0498f15e
chiboy <chiboygm1@gmail.com>
2016-08-01 12:18:53 +0800

    delete app2-timestamp-copy.herokuapp.com/

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,7 +5,7 @@ module.exports = function(app){
 	
 	res.sendFile(process.cwd()+"/public/index.html");
 });
-	app.route("/app2-timestamp-copy.herokuapp.com/:data").get(require(process.cwd()+"/app/controllers/date_deal.js")
+	app.route("/:data").get(require(process.cwd()+"/app/controllers/date_deal.js")
 		//res.send("i will send the human-convinent date");
 	);
 
